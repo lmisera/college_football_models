976f1c7fb84b1553e3d34909dfadfc17611264fa
lmisera <lmisera@umich.edu>
2024-10-28 08:47:30 -0400

    Add initial file

--- Streamlit_cfb.py
+++ Streamlit_cfb.py
@@ -1,40 +1,21 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[6]:
-
-
-import streamlit as st
-
-st.title("Streamlit Test")
-st.write("Hello, this is a test to see if Streamlit is working!")
-
-# Add a button
-if st.button("Click me!"):
-    st.write("Button clicked!")
-else:
-    st.write("Button not clicked yet.")
-
-
 # In[3]:
 
 
-"""
-
-%pip install streamlit
+#%pip install streamlit
 import streamlit as st
 import pandas as pd
 import os
 
 # Set the week number
 week = 10
-"""
 
 
 # In[4]:
 
 
-"""
 # Set up directory and read in the data
 current_dir = os.path.dirname(__file__)
 data_path = os.path.join(current_dir, 'data', f'Week {week}_no_outcomes.xlsx')
@@ -50,7 +31,6 @@ st.title(f'Week {week} games and predictions')
 
 # Selection box to choose game
 selected_game = st.selectbox('Game: ', df['game_for_list'])
-"""
 
 
 # In[5]:
